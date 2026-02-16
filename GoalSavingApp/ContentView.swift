30e33df636aca2c95f3b2cc0be16c71f51531e9d
Alikweyu1 <alikweyu1@gmail.com>
2026-02-16 17:51:43 +0300

    update Carousel

--- GoalSavingApp/ContentView.swift
+++ GoalSavingApp/ContentView.swift
@@ -48,8 +48,11 @@ struct ContentView: View {
                         isSetGoat  = true
                     }
                 }
+                        //MARK::Add Carousel
+                        VStack(alignment: .leading, spacing: 10) {
+                                    
                 
-                
+                    
             }
             
             .fullScreenCover(isPresented:$isSetGoat){
@@ -66,8 +69,84 @@ struct ContentView: View {
     func  navigate() -> some View {
         CreateSavinggoalView()
     }
+    private var headerSection: some View {
+            HStack(spacing: 15) {
+                Circle()
+                    .fill(Color.white.opacity(0.2))
+                    .frame(width: 50, height: 50)
+                    .overlay(Image(systemName: "person").foregroundColor(.white))
+                
+                VStack(alignment: .leading) {
+                    Text("Hello There!").font(.headline)
+                    Text("It's a good day to save").font(.subheadline).opacity(0.8)
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
+            .padding()
+            .background(Color("#063B27"))
+        }
+    private var goalSavingsHeroCard: some View {
+            Button(action: { isSetGoal = true }) {
+                HStack {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Goal Savings").font(.title3).bold()
+                        Text("Turn your goals into\nsavings!").font(.subheadline)
+                    }
+                    Spacer()
+                   
+                    RoundedRectangle(cornerRadius: 12)
+                        .fill(Color.white.opacity(0.2))
+                        .frame(width: 80, height: 80)
+                        .overlay(
+                            Image(systemName: "shield.fill")
+                                .font(.largeTitle)
+                        )
+                }
+                .padding(30)
+                .foregroundColor(.white)
+                .background(Color(hex: "#82C952"))
+                .cornerRadius(15)
+                .padding(.horizontal)
+            }
+        }
+    
 }
 
 //#Preview {
 //    ContentView()
 //}
+struct InfoBanner: View {
+    var title: String
+    var subtitle: String
+    var blockColor: Color
+    var bgColor: Color
+    
+    var body: some View {
+        HStack(spacing: 0) {
+           
+            Rectangle()
+                .fill(blockColor)
+                .frame(width: 80)
+            
+            // Right Side: Content
+            VStack(alignment: .leading, spacing: 8) {
+                Text(title)
+                    .font(.system(size: 14, weight: .bold))
+                    .foregroundColor(Color(hex: "#82C952"))
+                
+                Text(subtitle)
+                    .font(.system(size: 12))
+                    .foregroundColor(.white)
+                    .lineLimit(3)
+                
+                Spacer()
+            }
+            .padding()
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .background(bgColor)
+        }
+        .cornerRadius(12)
+        .clipped()
+    }
+}
