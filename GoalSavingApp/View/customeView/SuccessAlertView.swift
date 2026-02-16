23d71f009905ffe9fa6f7253767108a3072706f6
Alikweyu1 <alikweyu1@gmail.com>
2026-02-16 18:15:30 +0300

    create success view

---
+++ GoalSavingApp/View/customeView/SuccessAlertView.swift
@@ -0,0 +1,18 @@
+//
+//  SuccessAlertView.swift
+//  GoalSavingApp
+//
+//  Created by Ali Kweyu on 16/02/2026.
+//
+
+import SwiftUI
+
+struct SuccessAlertView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SuccessAlertView()
+}
